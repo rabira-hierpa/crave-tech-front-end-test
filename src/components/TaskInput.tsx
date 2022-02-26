b845a4cd45a8988440e6e8d21ca944ea1d10d8b6
Rabra Hierpa <rabira.hierpa@gmail.com>
2022-02-26 14:48:16 +0300

    ➕ added task input

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,22 +1,39 @@
+import { Checkbox, Form } from "antd";
 import React, { useEffect, useState } from "react";
 import { StartupTaskType } from "../lib/types/task.type";
 
+interface ITaskInput {
+  taskList: StartupTaskType[];
+}
+const TaskInput: React.FC<ITaskInput> = (props) => {
+  const [TaskForm] = Form.useForm();
+  const { taskList } = props;
+  useEffect(() => {}, []);
 
-const TaskInput:React.FC = () => {
-  const [taskList, setTaskList] = useState<StartupTaskType[]>([]);
-
-  useEffect(() => {
-	//@ts-ignore
-    const currentTaskList:StartupTaskType[] = JSON.parse(localStorage.getItem("taskList"))
-
-    if (currentTaskList) {
-      setTaskList(currentTaskList);
-    }
-  }, []);
-
-
-
-  return <div>TaskInput</div>;
+  return (
+    <div>
+      {taskList?.map((task) => {
+        return (
+          <div key={task.id}>
+            <h1>{task.title}</h1>
+            <Form.List name="sub_tasks" initialValue={task.sub_tasks}>
+              {(fields) => {
+                return (
+                  <div>
+                    {fields.map((field) => (
+                      <Form.Item {...field}>
+                        <Checkbox />
+                      </Form.Item>
+                    ))}
+                  </div>
+                );
+              }}
+            </Form.List>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default TaskInput;
