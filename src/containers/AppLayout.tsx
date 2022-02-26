5e20649f450f6f0c2ebb2a848faa73481eb58dda
Rabra Hierpa <rabira.hierpa@gmail.com>
2022-02-26 14:47:40 +0300

    🗼resturcutred app layout

---
+++ src/containers/AppLayout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppLayout = ({ children }: any) => {
+  return <div className="flex items-center justify-center bg-slate-200">{children}</div>;
+};
+
+export default AppLayout;
